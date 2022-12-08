9d8b64fcc047a16af2cd5d6fa5deeabd439984ec
Elijah Bassett <69734856+BassettUNC@users.noreply.github.com>
2022-12-07 21:59:23 -0500

    workin on the output

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import minimist from "minimist"
 import fetch from "node-fetch"
+// NOTES FOR Documentation: Must install these.
 
 const args = minimist(process.argv.slice(2));
 
@@ -20,8 +21,9 @@ if (args.h) {
 	process.exit(0);
 }
 
-// NOTES FOR Documentation: Can only seelct one arg at a time. 
+// NOTES FOR Documentation: Can only seelct one arg at a time. OR arg + j
 let argcount = 0;
+let url = '';
 
 if (args.r) {
     url = 'random.php';
@@ -51,32 +53,24 @@ if (args.n) {
     url = 'list.php?i=list'
     argcount += 1;
 }
+if (argcount > 1) {
+    console.log("You may only specify one argument per request.")
+    process.exit(0);
+}
 
-let baseUrl = "https://api.open-meteo.com/v1/forecast?";
-let url = baseUrl + 'latitude=' + latit + '&longitude=' + longit + "&timezone=" + timezone + "&daily=precipitation_hours";
+const baseUrl = 'https://www.themealdb.com/api/json/v1/1/' + url;
 
-const response = await fetch(url);
+const response = await fetch(baseUrl);
 const data = await response.json();
 
-if ("j" in args) {
+if (args.j) {
 	console.log(data);
 	process.exit(0);
 }
-
-let precipitationForecast = data.daily.precipitation_hours;
-let precipitation = precipitationForecast[dayOffset];
-let message = ""
-
-if (precipitation === 0) {
-	message += "You will not need your galoshes ";
+if (!data.meals) {
+    console.log ("No meals found using your search parameters :(")
+    process.exit(0);
 } else {
-	message += "You might need your galoshes ";
+    console.log("Meal Name: " + data.meals[0].strMeal + '\n')
+    console.log("Instructions: \n" + data.meals[0].strInstructions)
 }
-if (dayOffset === 0) {
-	message += 'today.';
-} else if (dayOffset === 1) {
-	message += 'tomorrow.';
-} else {
-	message += 'in ' + dayOffset + ' days.';
-}
-console.log(message);
